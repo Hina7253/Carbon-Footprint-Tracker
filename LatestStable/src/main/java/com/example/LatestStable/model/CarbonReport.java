e174388380508365d91242f49fc8cc0f4a19b68b
Hina <hinakumari.725394@gmail.com>
2026-03-02 21:54:39 +0530

    Add entity

--- LatestStable/src/main/java/com/example/LatestStable/model/CarbonReport.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/CarbonReport.java
@@ -1,9 +1,23 @@
 package com.example.LatestStable.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "carbon_reports")
+@Data
+@NoArgsConstructor
 
 public class CarbonReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String url;
     private double totalPageSize;
     private double co2PerVisitGrams;
@@ -11,4 +25,13 @@ public class CarbonReport {
     private String carbonGrade;
     private boolean greenHosted;
     private LocalDateTime scannedAt;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<PageResources> resources;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OptimizationSuggestion> suggestions;
+
+
+
 }
