1ac5bce6eac21ace520e0cdf11464dea05eea4f7
Hina <hinakumari.725394@gmail.com>
2026-03-02 20:37:16 +0530

    first commit

---
+++ LatestStable/src/test/java/com/example/LatestStable/LatestStableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LatestStable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatestStableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
