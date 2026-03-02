6dae6bc83fc67d6172fe449870d7d6e2c1c5ff6e
Hina <hinakumari.725394@gmail.com>
2026-03-02 21:35:15 +0530

    Initial commit

--- LatestStable/src/main/java/com/example/LatestStable/model/CarbonReport.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/CarbonReport.java
@@ -1,4 +1,14 @@
 package com.example.LatestStable.model;
 
+import java.time.LocalDateTime;
+
 public class CarbonReport {
+    private Long id;
+    private String url;
+    private double totalPageSize;
+    private double co2PerVisitGrams;
+    private double co2PerYearKg;
+    private String carbonGrade;
+    private boolean greenHosted;
+    private LocalDateTime scannedAt;
 }
