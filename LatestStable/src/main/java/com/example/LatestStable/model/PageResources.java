3f77246a210b7cdf9d20c35931525457edb0bf14
Hina <hinakumari.725394@gmail.com>
2026-03-03 08:19:00 +0530

    Add pageresource

--- LatestStable/src/main/java/com/example/LatestStable/model/PageResources.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/PageResources.java
@@ -1,4 +1,21 @@
 package com.example.LatestStable.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class PageResources {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String url;
+    private String resourceType;
+    private double sizeKB;
+    private double co2Contribution;
 }
