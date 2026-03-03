5fe371896278ac8ece630e179175ed0f90e34e56
Hina <hinakumari.725394@gmail.com>
2026-03-03 08:35:07 +0530

    add variables

--- LatestStable/src/main/java/com/example/LatestStable/model/OptimizationSuggestion.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/OptimizationSuggestion.java
@@ -1,4 +1,11 @@
 package com.example.LatestStable.model;
 
 public class OptimizationSuggestion {
+    private Long id;
+    private String Category;
+    private String issue;
+    private String suggestion;
+    private String impact;
+    private double potentialSavingKb;
+
 }
