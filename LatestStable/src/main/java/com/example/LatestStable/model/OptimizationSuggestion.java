e16ef82bae48c98dda507a9896239bb8f4818d3e
Hina <hinakumari.725394@gmail.com>
2026-03-03 08:40:21 +0530

    update optimizationsuggestion

--- LatestStable/src/main/java/com/example/LatestStable/model/OptimizationSuggestion.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/OptimizationSuggestion.java
@@ -1,11 +1,24 @@
 package com.example.LatestStable.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class OptimizationSuggestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Category;
-    private String issue;
-    private String suggestion;
-    private String impact;
+    private String Category;  // IMAGE, JAVASCRIPT, CACHING etc
+    private String issue;     // "Uncompressed images found"
+    private String suggestion;  //"Convert to WebP format"
+    private String impact;       // HIGH, MEDIUM, LOW
     private double potentialSavingKb;
 
 }
