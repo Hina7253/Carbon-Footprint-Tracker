f3649892d5b4c0423eae60bd5947f130467ec543
Hina <hinakumari.725394@gmail.com>
2026-03-04 16:03:49 +0530

    add service

---
+++ LatestStable/src/main/java/com/example/LatestStable/service/PageAuditService.java
@@ -0,0 +1,18 @@
+package com.example.LatestStable.service;
+
+import com.example.LatestStable.model.PageResources;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageAuditService {
+    public List<PageResources> auditPage(String url) {
+        List<PageResources> resources = new ArrayList<>();
+
+        // Chrome Options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+    }
+}
