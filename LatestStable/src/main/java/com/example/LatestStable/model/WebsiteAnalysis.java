7f881cadda2b5cc95b3149dc9145f6f49322b7bc
Hina <hinakumari.725394@gmail.com>
2026-03-06 23:39:40 +0530

    update websiteanalysis

--- LatestStable/src/main/java/com/example/LatestStable/model/WebsiteAnalysis.java
+++ LatestStable/src/main/java/com/example/LatestStable/model/WebsiteAnalysis.java
@@ -1,4 +1,32 @@
 package com.example.LatestStable.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "website_analyses")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WebsiteAnalysis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String websiteUrl;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Long monthlyVisits = 10000L;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer crawlPages = 1;
+
+
 }
