56c9e9cca5368129fae7b38ee91d621eda5f39e5
Hina <hinakumari.725394@gmail.com>
2026-03-09 22:28:46 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
@@ -1,4 +1,10 @@
 package com.example.LatestStable.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AnalysisService {
 }
