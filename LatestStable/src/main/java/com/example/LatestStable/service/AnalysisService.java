8fe93032cd6ec3a648bbe47faf0a521dcd3bc01a
Hina <hinakumari.725394@gmail.com>
2026-03-09 22:31:00 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
@@ -1,5 +1,8 @@
 package com.example.LatestStable.service;
 
+import com.example.LatestStable.repository.PageResourcesRepository;
+import com.example.LatestStable.repository.WebsiteAnalysisRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +10,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class AnalysisService {
+    private final WebsiteAnalysisRepository analysisRepository;
+    private final PageResourcesRepository resourceRepository;
+    private final WebCrawlerService crawlerService;
+    private final CarbonCalculatorService carbonCalculatorService;
 }
