b5cf612486be811a8ece6dc4fcc4fe045b1c15e3
Hina <hinakumari.725394@gmail.com>
2026-03-10 22:25:18 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/AnalysisService.java
@@ -19,22 +19,9 @@ import java.util.stream.Collectors;
 
 @Service
 
-@RequiredArgsConstructor
+
 public class AnalysisService {
-    private static final org.slf4j.Logger log =
-            org.slf4j.LoggerFactory.getLogger(AnalysisService.class);
 
-    private final WebsiteAnalysisRepository analysisRepository;
-    private final PageResourcesRepository resourceRepository;
-    private final WebCrawlerService crawlerService;
-    private final CarbonCalculatorService carbonCalculatorService;
-
-    public AnalysisService(WebsiteAnalysisRepository analysisRepository, PageResourcesRepository resourceRepository, WebCrawlerService crawlerService, CarbonCalculatorService carbonCalculatorService) {
-        this.analysisRepository = analysisRepository;
-        this.resourceRepository = resourceRepository;
-        this.crawlerService = crawlerService;
-        this.carbonCalculatorService = carbonCalculatorService;
-    }
 
     @Transactional
     public AnalysisResponseDTO startAnalysis(AnalysisRequestDTO request) {
