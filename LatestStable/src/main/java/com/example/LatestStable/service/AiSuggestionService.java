f2156acee6338d95e8f04fc18324ef39af44f364
Hina <hinakumari.725394@gmail.com>
2026-03-12 21:50:30 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/AiSuggestionService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/AiSuggestionService.java
@@ -1,4 +1,11 @@
 package com.example.LatestStable.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AiSuggestionService {
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(AiSuggestionService.class);
+
+
 }
