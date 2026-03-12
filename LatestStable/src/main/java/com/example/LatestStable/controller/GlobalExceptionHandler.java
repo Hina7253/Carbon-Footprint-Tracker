2a79cde2b069c8b9f9e6387b6c02051c46ab8081
Hina <hinakumari.725394@gmail.com>
2026-03-12 21:35:44 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/controller/GlobalExceptionHandler.java
+++ LatestStable/src/main/java/com/example/LatestStable/controller/GlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.example.LatestStable.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // VALIDATION ERRORS
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
 }
