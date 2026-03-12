417173cd220d8a547ae1fe75b13450c9f96bf92d
Hina <hinakumari.725394@gmail.com>
2026-03-12 21:00:06 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/controller/AnalysisController.java
+++ LatestStable/src/main/java/com/example/LatestStable/controller/AnalysisController.java
@@ -1,5 +1,6 @@
 package com.example.LatestStable.controller;
 
+import com.example.LatestStable.service.AnalysisService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnalysisController {
     private static final org.slf4j.Logger log =
             org.slf4j.LoggerFactory.getLogger(AnalysisController.class);
+    private final AnalysisService analysisService;
+
+    public AnalysisController(AnalysisService analysisService) {
+        this.analysisService = analysisService;
+    }
 }
