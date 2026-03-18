96c0ef38b36154820b8ac62188d523e3a7a7feeb
Hina <hinakumari.725394@gmail.com>
2026-03-18 20:13:47 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/LeaderobardService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/LeaderobardService.java
@@ -1,4 +1,15 @@
 package com.example.LatestStable.service;
 
+import com.example.LatestStable.repository.WebsiteAnalysisRepository;
+
 public class LeaderobardService {
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(LeaderboardService.class);
+
+    private final WebsiteAnalysisRepository analysisRepository;
+
+    public LeaderboardService(
+            WebsiteAnalysisRepository analysisRepository) {
+        this.analysisRepository = analysisRepository;
+    }
 }
