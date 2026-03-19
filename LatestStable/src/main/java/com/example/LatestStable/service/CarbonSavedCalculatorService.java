e72eb278b64d72dd2872b1a91c12ef12a09dae29
Hina <hinakumari.725394@gmail.com>
2026-03-19 22:29:13 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/service/CarbonSavedCalculatorService.java
+++ LatestStable/src/main/java/com/example/LatestStable/service/CarbonSavedCalculatorService.java
@@ -1,4 +1,14 @@
 package com.example.LatestStable.service;
 
+import com.example.LatestStable.repository.PageResourcesRepository;
+import com.example.LatestStable.repository.WebsiteAnalysisRepository;
+
 public class CarbonSavedCalculatorService {
+    rivate static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(
+                    CarbonSavedCalculatorService.class);
+
+    private final WebsiteAnalysisRepository analysisRepository;
+    private final PageResourcesRepository resourceRepository;
+    private final CarbonCalculatorService carbonCalculatorService;
 }
