1b93c3524b4fa8038fe178dffd02479dd58b9660
Hina <hinakumari.725394@gmail.com>
2026-03-19 21:20:57 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/controller/ExtraFeaturesController.java
+++ LatestStable/src/main/java/com/example/LatestStable/controller/ExtraFeaturesController.java
@@ -1,6 +1,9 @@
 package com.example.LatestStable.controller;
 
 
+import com.example.LatestStable.service.BadgeService;
+import com.example.LatestStable.service.CompareService;
+import com.example.LatestStable.service.IndustryCompareService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +15,9 @@ public class ExtraFeaturesController {
     private static final org.slf4j.Logger log =
             org.slf4j.LoggerFactory.getLogger(
                     ExtraFeaturesController.class);
+
+    private final CompareService compareService;
+    private final LeaderboardService leaderboardService;
+    private final BadgeService badgeService;
+    private final IndustryCompareService industryCompareService;
 }
