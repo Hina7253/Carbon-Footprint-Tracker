8114f4597778340c451418d2ce891725cd8a2a2e
Hina <hinakumari.725394@gmail.com>
2026-03-22 22:12:30 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/controller/EmailController.java
+++ LatestStable/src/main/java/com/example/LatestStable/controller/EmailController.java
@@ -1,4 +1,11 @@
 package com.example.LatestStable.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/analyses")
+@CrossOrigin(origins = "*")
 public class EmailController {
 }
