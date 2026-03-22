de415839135f22a93d8fd86dc720fe3cfdeefe22
Hina <hinakumari.725394@gmail.com>
2026-03-22 22:13:12 +0530

    update

--- LatestStable/src/main/java/com/example/LatestStable/controller/EmailController.java
+++ LatestStable/src/main/java/com/example/LatestStable/controller/EmailController.java
@@ -1,5 +1,7 @@
 package com.example.LatestStable.controller;
 
+import com.example.LatestStable.service.EmailService;
+import com.example.LatestStable.service.PdfReportService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/analyses")
 @CrossOrigin(origins = "*")
 public class EmailController {
+
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(EmailController.class);
+
+    private final EmailService emailService;
+    private final PdfReportService pdfReportService;
+
+    public EmailController(
+            EmailService emailService,
+            PdfReportService pdfReportService) {
+        this.emailService    = emailService;
+        this.pdfReportService = pdfReportService;
+    }
+
 }
